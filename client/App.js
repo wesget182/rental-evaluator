2582c3b909de281d0b1107f7bd694a62c4000212
aramay <kasper_341@hotmail.com>
2021-06-23 19:28:42 -0700

    add App component

---
+++ client/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import LandingPage from '../FE/LandingPage';
+import { Container, Jumbotron, Navbar } from 'react-bootstrap';
+import DeckList from '../FE/DecksList';
+import { DeckCards } from '../FE/DeckCards';
+import { ReviewCards }  from '../FE/ReviewCards';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+
+const App = () => {
+
+  return (
+    <div>rental evaluator</div>
+  );
+};
+
+export default App;
