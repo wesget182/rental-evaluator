7bb5bfb1d0b0941693adec80cadca53a79f5f6a5
aramay <kasper_341@hotmail.com>
2021-06-25 11:03:58 -0700

    import React, MapGL packages

---
+++ client/MapView.js
@@ -0,0 +1,10 @@
+// https://mariestarck.com/how-to-display-a-mapbox-map-and-geocoder-mapbox-react-tutorial-part-1/
+
+import React, { PureComponent } from 'react';
+import ReactMapGL from 'react-map-gl';
+import { Container, Col, Row } from 'reactstrap';
+
+const mapStyle = {
+    width: '100%',
+    height: 600
+}
