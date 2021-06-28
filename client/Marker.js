7e913c9ae1068fe73a1f5e67a9d893cafdd49edc
aramay <kasper_341@hotmail.com>
2021-06-27 18:41:00 -0700

    rename file Marker.js to MarkersList.js

--- client/Marker.js
+++
@@ -1,8 +0,0 @@
-import { Marker } from 'mapbox-gl';
-import React from 'react';
-import ReactMapGL from 'react-map-gl';
-
-
-
-export default Marker;
-
---
+++ client/MarkersList.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+const MarkersList = (props) => {
+
+  cons
+  const [viewport, setViewport] = useState({
+    longitude: -121.27096757069442,
+    latitude: 36.23291459044428,
+    zoom: 12,
+    bearing: 0,
+    pitch: 0
+  });
+
+
+
+}
+
+export default Marker;
