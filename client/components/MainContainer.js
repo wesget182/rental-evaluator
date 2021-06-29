54463a3e55840fc606e16a4517710b5dba82aa32
Nick <nandreala@gmail.com>
2021-06-28 21:17:30 -0400

    favs view, navbar

--- client/components/MainContainer.js
+++ client/components/MainContainer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import NavBar from './NavBar';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
 
 const MainContainer = () => {
   return (
     <div>
-      <p>main display</p>
+      <Container>
+        <Box>
+          <Card>search field</Card>
+        </Box>
+        <Box>map</Box>
+      </Container>
     </div>
   );
 };
