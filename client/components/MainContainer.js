4f3761b61a5d1189a12ae803b394b36bae0a2caa
Nick <nandreala@gmail.com>
2021-06-28 23:09:32 -0400

    merging changes

--- client/components/MainContainer.js
+++ client/components/MainContainer.js
@@ -7,27 +7,26 @@ import Card from '@material-ui/core/Card';
 const MainContainer = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Container>
         <Box>
           <Card>search field</Card>
         </Box>
         <Box>map</Box>
       </Container>
-=======
-      <p>main display</p>
+      {/* <p>main display</p>
       <div>rental evaluator</div>
-      <button onClick={ async () => {
-        let res = await fetch('/api/clickMe');
-        res = await res.json();
-        console.log(JSON.stringify(res));
-        const node = document.getElementById('listings');
-        node.innerHTML = JSON.stringify(res);
-      } }>
+      <button
+        onClick={async () => {
+          let res = await fetch('/api/clickMe');
+          res = await res.json();
+          console.log(JSON.stringify(res));
+          const node = document.getElementById('listings');
+          node.innerHTML = JSON.stringify(res);
+        }}
+      >
         Click me
       </button>
-      <div id='listings'></div>
->>>>>>> dev
+      <div id="listings"></div> */}
     </div>
   );
 };
