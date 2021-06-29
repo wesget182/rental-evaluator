d000a4e36b7ea623d6dc11e3cc699a4604602bc7
aramay <kasper_341@hotmail.com>
2021-06-29 13:00:14 -0700

    fix fomatting

--- client/SearchBox.js
+++ client/SearchBox.js
@@ -6,21 +6,20 @@ const SearchBox = () => {
 
   return (
     <section>
-          <form>
-            <label htmlFor="searchBox">Search ...</label>
-            <input
-              type="text"
-              id="searchBox"
-              name="searchBox"
-              placeholder="What's on your mind?"
-              value={title}
-              onChange={onTitleChanged}
-            />
-          </form>
-          <button type="button" onClick={onSavePostClicked}>
-            Search
-          </button>
-        </section>
-
+      <form>
+        <label htmlFor="searchBox">Search ...</label>
+        <input
+          type="text"
+          id="searchBox"
+          name="searchBox"
+          placeholder="What's on your mind?"
+          value={title}
+          onChange={onSearchChanged}
+        />
+      </form>
+      <button type="button" onClick={onSearchClicked}>
+        Search
+      </button>
+    </section>
   )
 }
