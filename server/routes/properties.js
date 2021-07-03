7ada0e4f7e197202a9a026b12326e173de0cac0b
kenchlo99 <kenchlo@comcast.net>
2021-07-03 00:18:17 -0700

    completed modal rendering with 2nd Zillow API when needed, and fixed the handler for the input filters

--- server/routes/properties.js
+++ server/routes/properties.js
@@ -38,16 +38,32 @@ router.all(
 );
 
 // handler for a clicked address search on a property pinned on the map
-router.get(
+router.post(
   '/target',
   (req, res, next) => {
+    res.locals.targetForSale = {
+      features:
+        [{
+          properties: {
+            Price: req.query.Price,
+            ZPID: req.query.ZPID
+          }
+        }]
+    };
     Object.assign(req.params, {
-      location: req.query.location,
-      // status_type: 'ForRent',
+      location: req.query.location.slice(-5),
+      status_type: 'ForRent',
+      home_type: req.query.home_type,
+      bedsMin: req.query.beds,
+      bedsMax: req.query.beds,
+      bathsMin: req.query.baths,
+      bathsMax: req.query.baths
     });
     middlewares.getPropertiesForRental(req, res, next);
   },
   (req, res) => {
+    console.log('Response of target endpoint:')
+    console.log(JSON.stringify(res.locals, null, 2));
     return res.status(200).json(res.locals);
   }
 );
