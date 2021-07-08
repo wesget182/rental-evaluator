30ec8dc57e11e7cd56e4b57b3351a79f589aa560
dyeoman2 <dyeoman2@gmail.com>
2021-07-07 18:28:18 -0700

    db connection and webpack cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,13 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-console.log(' ## path ##');
-console.log(__dirname + '/index.html');
-const htmlPlugin = new HtmlWebPackPlugin({
-  // path to build index.html in /build
-  template: './index.html',
-});
+// path to build index.html in /build
+const htmlPlugin = new HtmlWebPackPlugin({ template: './index.html' });
 
 module.exports = {
   devtool: 'eval-source-map',
   mode: 'production',
   entry: ['./index.js'],
-
   output: {
     filename: 'bundle.[fullhash].js',
     path: path.resolve(__dirname, 'dist'),
@@ -23,7 +18,6 @@ module.exports = {
     rules: [
       {
         test: /.(js|jsx)$/,
-        // test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -45,29 +39,18 @@ module.exports = {
         ],
       },
       {
-        // test: /\.css$/,
         test: /.(css|scss)$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
   },
-  plugins: [
-    htmlPlugin,
-    new webpack.HotModuleReplacementPlugin({
-      // Options...
-    }),
-  ],
+  plugins: [htmlPlugin, new webpack.HotModuleReplacementPlugin({})],
   devServer: {
-    // host: 'localhost',
-    // port: 8080,
-    // contentBase: path.resolve(__dirname, '/dist'),
-    // compress: true,
     publicPath: '/',
     hot: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
     // fallback to root for other urls
     historyApiFallback: true,
-
     //setup proxy to access BE server
     proxy: {
       '/api': {
